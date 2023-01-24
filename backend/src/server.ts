cfdb5548faaabfa2f1efd9b9dcbe5ca727f6b038
ElhanM <elhan.mahmutovic03@gmail.com>
2023-01-24 22:07:50 +0100

    bootstrapped backend

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import app from "./app";
+import db from "./models";
+
+const port = process.env.PORT || 5000;
+
+// IIFE
+(async () => {
+  try {
+    await db.sequelize.sync({ alter: true });
+    console.log("Database sync...");
+    app.listen(port, () => console.log(`Server is listening port ${port}...`));
+  } catch (error) {
+    console.log("server.js error:", error);
+  }
+})();
