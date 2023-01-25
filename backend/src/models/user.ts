03732d59d1f21ad552fc611dd8a6c9bd29f2e307
ElhanM <elhan.mahmutovic03@gmail.com>
2023-01-25 21:44:19 +0100

    started working on models

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,25 +1,74 @@
 import { DataTypes, Model } from "sequelize";
 import db from "config/config";
-import { v4 as uuidv4 } from "uuid";
-
+import { Post } from "./post";
 interface IUser {
-  id?: string;
+  userId?: string;
   username: string;
+  email: string;
+  password: string;
 }
 
-export class UserInstance extends Model<IUser> {}
+export class User extends Model<IUser> {}
 
-UserInstance.init(
+User.init(
   {
-    id: {
+    userId: {
       type: DataTypes.UUID,
       primaryKey: true,
       allowNull: false,
-      defaultValue: uuidv4(),
+      defaultValue: DataTypes.UUIDV4,
     },
     username: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: "Username already exists",
+      validate: {
+        notEmpty: {
+          msg: "Username is required",
+        },
+        len: {
+          args: [4, 25],
+          msg: "Username must be between 1 and 25 characters",
+        },
+        is: {
+          args: /^[a-zA-Z0-9_.-]+$/,
+          msg: "Username can only contain letters, numbers, dash, underscore and dot",
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: "Username already exists",
+      validate: {
+        notEmpty: {
+          msg: "Email is required",
+        },
+        isEmail: {
+          msg: "Invalid email",
+        },
+        len: {
+          args: [4, 255],
+          msg: "Email must be between 4 and 255 characters",
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Password is required",
+        },
+        len: {
+          args: [8, 32],
+          msg: "Password must be between 8 and 32 characters",
+        },
+        is: {
+          args: /^[a-zA-Z0-9_.-]+$/,
+          msg: "Password can only contain letters, numbers, dash, underscore and dot",
+        },
+      },
     },
   },
   {
@@ -27,3 +76,16 @@ UserInstance.init(
     tableName: "users",
   },
 );
+
+// this adds the userId column to the posts table
+// User.hasMany(Post, {
+//   foreignKey: {
+//     name: "userId",
+//     // type: Sequelize.DataTypes.UUID,
+//     allowNull: false,
+//   },
+//   onDelete: "CASCADE",
+//   onUpdate: "CASCADE",
+// });
+// pass all of the same options to both
+// does the same thing as line above, but this way we have utility methods both ways
