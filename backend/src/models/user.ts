11d2c8aae70ef36cfbcf752ddb88ccf39bb00607
ElhanM <elhan.mahmutovic03@gmail.com>
2023-01-26 12:05:47 +0100

    made some progress setting up sequelize with typescript, relations sort of work now

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,91 +1,102 @@
-import { DataTypes, Model } from "sequelize";
-import db from "config/config";
-import { Post } from "./post";
-interface IUser {
+"use strict";
+import { Model } from "sequelize";
+
+interface UserAttributes {
   userId?: string;
   username: string;
   email: string;
   password: string;
 }
 
-export class User extends Model<IUser> {}
-
-User.init(
-  {
-    userId: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      allowNull: false,
-      defaultValue: DataTypes.UUIDV4,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: "Username already exists",
-      validate: {
-        notEmpty: {
-          msg: "Username is required",
+module.exports = (sequelize: any, DataTypes: any) => {
+  class User extends Model<UserAttributes> implements UserAttributes {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    userId!: string;
+    username!: string;
+    email!: string;
+    password!: string;
+    static associate(models: any) {
+      User.hasMany(models.Post, {
+        foreignKey: {
+          name: "userId",
+          // type: Sequelize.DataTypes.UUID,
+          allowNull: false,
         },
-        len: {
-          args: [4, 25],
-          msg: "Username must be between 1 and 25 characters",
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      });
+    }
+  }
+  User.init(
+    {
+      userId: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: "Username already exists",
+        validate: {
+          notEmpty: {
+            msg: "Username is required",
+          },
+          len: {
+            args: [4, 25],
+            msg: "Username must be between 1 and 25 characters",
+          },
+          is: {
+            args: /^[a-zA-Z0-9_.-]+$/,
+            msg: "Username can only contain letters, numbers, dash, underscore and dot",
+          },
         },
-        is: {
-          args: /^[a-zA-Z0-9_.-]+$/,
-          msg: "Username can only contain letters, numbers, dash, underscore and dot",
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: "Username already exists",
+        validate: {
+          notEmpty: {
+            msg: "Email is required",
+          },
+          isEmail: {
+            msg: "Invalid email",
+          },
+          len: {
+            args: [4, 255],
+            msg: "Email must be between 4 and 255 characters",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Password is required",
+          },
+          len: {
+            args: [8, 32],
+            msg: "Password must be between 8 and 32 characters",
+          },
+          is: {
+            args: /^[a-zA-Z0-9_.-]+$/,
+            msg: "Password can only contain letters, numbers, dash, underscore and dot",
+          },
         },
       },
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: "Username already exists",
-      validate: {
-        notEmpty: {
-          msg: "Email is required",
-        },
-        isEmail: {
-          msg: "Invalid email",
-        },
-        len: {
-          args: [4, 255],
-          msg: "Email must be between 4 and 255 characters",
-        },
-      },
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "users",
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: {
-          msg: "Password is required",
-        },
-        len: {
-          args: [8, 32],
-          msg: "Password must be between 8 and 32 characters",
-        },
-        is: {
-          args: /^[a-zA-Z0-9_.-]+$/,
-          msg: "Password can only contain letters, numbers, dash, underscore and dot",
-        },
-      },
-    },
-  },
-  {
-    sequelize: db,
-    tableName: "users",
-  },
-);
-
-// this adds the userId column to the posts table
-// User.hasMany(Post, {
-//   foreignKey: {
-//     name: "userId",
-//     // type: Sequelize.DataTypes.UUID,
-//     allowNull: false,
-//   },
-//   onDelete: "CASCADE",
-//   onUpdate: "CASCADE",
-// });
-// pass all of the same options to both
-// does the same thing as line above, but this way we have utility methods both ways
+  );
+  return User;
+};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,48 +1,50 @@
 import app from "app";
-import db from "config/config";
-import { User } from "models/user";
-import { Post } from "models/post";
+import db from "models";
 
 const port = process.env.PORT || 5000;
 
 // IIFE
 (async () => {
   try {
-    await db.sync({ force: true });
+    await db.sequelize.sync({ force: true });
     console.log("Database synced!");
     // bulk create 3 users
-    await User.bulkCreate([
+    await db.User.bulkCreate([
       {
+        userId: "97cbf1a0-5b1f-11eb-ae93-0242ac130002",
         username: "user1",
         email: "user1@gmail.com",
         password: "user1",
       },
       {
+        userId: "97cbf1a0-5b1f-11eb-ae93-0242ac130003",
         username: "user2",
         email: "user2@gmail.com",
         password: "user2",
       },
     ]);
     // bulk create 2 posts
-    await Post.bulkCreate([
+    await db.Post.bulkCreate([
       {
+        postId: "97cbf1a0-5b1f-11eb-ae93-0242ac130004",
         title: "post1",
         description: "post1 description",
       },
       {
+        postId: "97cbf1a0-5b1f-11eb-ae93-0242ac130005",
         title: "post2",
         description: "post2 description",
       },
     ]);
 
-    const post = await Post.findOne({ where: { title: "post1" } });
-    const user = await User.findOne({ where: { username: "user1" } });
+    const post = await db.Post.findOne({ where: { title: "post1" } });
+    const user = await db.User.findOne({ where: { username: "user1" } });
     console.log("post:", post?.toJSON());
     console.log("user:", user?.toJSON());
-
-    // if (post && user) {
-    //   await post.setUser(user);
-    // }
+    // associate user with post
+    await user?.addPost(post);
+    // const postAss = await db.Post.findOne({ where: { title: "post1" } });
+    // console.log("postAss:", postAss?.toJSON());
 
     app.listen(port, () => console.log(`Server is listening port ${port}...`));
   } catch (error) {
