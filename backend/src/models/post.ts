8b17a4a98cde63a4198e99a2076670c7add7ee21
ElhanM <elhan.mahmutovic03@gmail.com>
2023-01-26 12:05:47 +0100

    made some progress setting up sequelize with typescript, relations sort of work now

--- backend/src/models/post.ts
+++ backend/src/models/post.ts
@@ -1,62 +1,84 @@
-import { DataTypes, Model } from "sequelize";
-import db from "config/config";
-import { User } from "./user";
+"use strict";
+import { Model } from "sequelize";
 
-interface IPost {
+interface PostAttributes {
   postId?: string;
   title: string;
   description: string;
+  userId?: string;
 }
 
-export class Post extends Model<IPost> {}
-
-Post.init(
-  {
-    postId: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      allowNull: false,
-      defaultValue: DataTypes.UUIDV4,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: {
-          msg: "Title is required",
+module.exports = (sequelize: any, DataTypes: any) => {
+  class Post extends Model<PostAttributes> implements PostAttributes {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    postId!: string;
+    title!: string;
+    description!: string;
+    userId!: string;
+    static associate(models: any) {
+      Post.belongsTo(models.Post, {
+        foreignKey: {
+          name: "userId",
+          // type: Sequelize.DataTypes.UUID,
+          // allowNull: false,
         },
-        len: {
-          args: [1, 255],
-          msg: "Title must be between 1 and 255 characters",
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      });
+    }
+  }
+  Post.init(
+    {
+      postId: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Title is required",
+          },
+          len: {
+            args: [1, 255],
+            msg: "Title must be between 1 and 255 characters",
+          },
+        },
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Description is required",
+          },
+          len: {
+            args: [1, 255],
+            msg: "Description must be between 1 and 255 characters",
+          },
+        },
+      },
+      userId: {
+        type: DataTypes.UUID,
+        // allowNull: false,
+        references: {
+          model: "users",
+          key: "userId",
         },
       },
     },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: {
-          msg: "Description is required",
-        },
-        len: {
-          args: [1, 255],
-          msg: "Description must be between 1 and 255 characters",
-        },
-      },
+    {
+      sequelize,
+      modelName: "Post",
+      tableName: "posts",
     },
-  },
-  {
-    sequelize: db,
-    tableName: "posts",
-  },
-);
-
-// Post.belongsTo(User, {
-//   foreignKey: {
-//     name: "userId",
-//     // type: Sequelize.DataTypes.UUID,
-//     allowNull: false,
-//   },
-//   onDelete: "CASCADE",
-//   onUpdate: "CASCADE",
-// });
+  );
+  return Post;
+};
