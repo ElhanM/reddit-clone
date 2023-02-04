8b97cb3caebf767a18a0131dde42746fb1538798
ElhanM <elhan.mahmutovic03@gmail.com>
2023-02-04 12:08:23 +0100

    started working on create-post page

--- frontend/src/components/pages/index.ts
+++ frontend/src/components/pages/index.ts
@@ -1,2 +1,3 @@
 export { default as Login } from "./auth/login/Login";
 export { default as Home } from "./posts/home/Home";
+export { default as CreatePostPage } from "./posts/create-post/CreatePostPage";
---
+++ frontend/src/components/pages/posts/create-post/CreatePostPage.tsx
@@ -0,0 +1,26 @@
+// PLUGINS IMPORTS //
+
+// COMPONENTS IMPORTS //
+import { CreatePost, CreatePostAside } from "components/templates";
+
+// EXTRA IMPORTS //
+import styles from "./create-post-page.module.css";
+
+/////////////////////////////////////////////////////////////////////////////
+
+type CreatePostPageProps = {};
+
+const CreatePostPage = (props: CreatePostPageProps) => {
+  return (
+    <article className={`${styles["create-post-wrapper"]}`}>
+      <main>
+        <CreatePost />
+      </main>
+      <aside>
+        <CreatePostAside />
+      </aside>
+    </article>
+  );
+};
+
+export default CreatePostPage;
