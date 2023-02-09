7a6d2e335ed12efb7f8eeb51f1fca331e93f80c9
ElhanM <elhan.mahmutovic03@gmail.com>
2023-02-09 17:16:12 +0100

    worked more on navbar

--- frontend/src/components/molecules/index.ts
+++ frontend/src/components/molecules/index.ts
@@ -3,3 +3,7 @@ export { default as Upvote } from "./upvote/Upvote";
 export { default as RSlash } from "./r-slash/RSlash";
 export { default as FormattedRMD } from "./formatted-rmd/FormattedRMD";
 export { default as ConfigedRQuill } from "./configed-r-quill/ConfigedRQuill";
+
+export { default as RedditIcon } from "./logo/reddit-icon/RedditIcon";
+export { default as RedditText } from "./logo/reddit-text/RedditText";
+export { default as RedditLogo } from "./logo/reddit-logo/RedditLogo";
