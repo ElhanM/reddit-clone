b4d3a37d91504bf7a3e83329fb3a61d82de3c6ea
ElhanM <elhan.mahmutovic03@gmail.com>
2023-02-18 23:17:39 +0100

    added create post aside modal

--- frontend/src/components/organisms/home-aside/HomeAside.tsx
+++ frontend/src/components/organisms/home-aside/HomeAside.tsx
@@ -18,7 +18,7 @@ const HomeAside = (props: HomeAsideProps) => {
   return (
     <aside>
       <Paper className={`${styles["home_aside-wrapper"]}`}>
-        <div className={`${styles["background"]}`}></div>
+        <div className={`${styles["background"]}`}/>
         <header className={`${styles["header"]}`}>
           <Avatar size={EAvatar.MEDIUM} translate />
           <Typography>Home</Typography>
--- frontend/src/components/templates/create-post-aside/CreatePostAside.tsx
+++ frontend/src/components/templates/create-post-aside/CreatePostAside.tsx
@@ -1,15 +1,49 @@
 // PLUGINS IMPORTS //
+import { Paper, Typography } from "@mui/material";
+import { ETheme } from "types/theme";
 
 // COMPONENTS IMPORTS //
+import { CreatePostAvatar } from "components/molecules";
 
 // EXTRA IMPORTS //
+import styles from "./create-post-aside.module.css";
 
 /////////////////////////////////////////////////////////////////////////////
 
 type CreatePostAsideProps = {};
 
+const rules = [
+  "Remember the human",
+  "Behave like you would in real life",
+  "Look for the original source of content",
+  "Search for duplicates before posting",
+  "Read the community's rules",
+];
+
 const CreatePostAside = (props: CreatePostAsideProps) => {
-  return <div>CreatePostAside</div>;
+  return (
+    <aside>
+      <Paper className={`${styles["create_post_aside-wrapper"]}`}>
+        <header className={`${styles["header"]}`}>
+          <CreatePostAvatar />
+          <Typography variant="h6" sx={{ fontSize: "1rem" }}>
+            Posting to Reddit
+          </Typography>
+        </header>
+        <div className={`${styles["border-seperate"]}`}></div>
+        <main>
+          {rules.map((rule, index) => (
+            <div key={index} className={`${styles["rule"]}`}>
+              <Typography variant="subtitle1" sx={{ fontSize: ".9rem" }}>
+                {index + 1}. {rule}
+              </Typography>
+              <div className={`${styles["border-seperate"]}`}></div>
+            </div>
+          ))}
+        </main>
+      </Paper>
+    </aside>
+  );
 };
 
 export default CreatePostAside;
